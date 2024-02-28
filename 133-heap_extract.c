55b3ccf231b26f5e43683b0860d4a0cd068e996c
Eseteselase <esete.s.berhanu@gmail.com>
2024-02-28 11:45:39 +0300

    fix

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -1,13 +1,35 @@
 #include "binary_trees.h"
 
 /**
- * heap_extract - extracts the root node of a Max Binary Heap
- * @root: a double pointer to the root node of heap
+ * heap_extract - Extracts the root node of a Max Binary Heap.
+ * @root: Double pointer to the root node of the heap.
  *
- * Return: the value stored in the root node
- *         0 on failure
+ * Return: Value stored in the root node.
  */
-int heap_extract(heap_t **root)
-{
 
+int heap_extract(heap_t **root) {
+    if (!root || !*root) {
+	    return 0;
+    }
+
+    heap_t *last_node = *root;
+    int last_level = 0;
+
+    while (last_node->left || last_node->right) {
+        last_level++;
+        if (!last_node->left) {
+            last_node = last_node->right;
+        } else {
+            last_node = last_node->left;
+        }
+    }
+
+    int value = (*root)->n;
+    free(*root);
+
+    *root = last_node;
+
+    heapify(*root, last_level, 0);
+
+    return value;
 }
