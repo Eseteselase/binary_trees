755df62d37dec2054dd21ca4839ab65e3c64f25a
Eseteselase <esete.s.berhanu@gmail.com>
2024-02-28 11:48:34 +0300

    fix2

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -6,30 +6,88 @@
  *
  * Return: Value stored in the root node.
  */
+int heap_extract(heap_t **root)
+{
+    int extracted_value;
 
-int heap_extract(heap_t **root) {
-    if (!root || !*root) {
-	    return 0;
-    }
+    if (root == NULL || *root == NULL)
+        return 0;
 
-    heap_t *last_node = *root;
-    int last_level = 0;
+    /* Extract value from the root node */
+    extracted_value = (*root)->n;
 
-    while (last_node->left || last_node->right) {
-        last_level++;
-        if (!last_node->left) {
-            last_node = last_node->right;
-        } else {
-            last_node = last_node->left;
+    /* Find the last level-order node */
+    while ((*root)->left || (*root)->right)
+    {
+        if ((*root)->right == NULL || (*root)->left->n > (*root)->right->n)
+        {
+            (*root)->n = (*root)->left->n;
+            *root = (*root)->left;
+        }
+        else
+        {
+            (*root)->n = (*root)->right->n;
+            *root = (*root)->right;
         }
     }
 
-    int value = (*root)->n;
-    free(*root);
+    /* Free the last level-order node */
+    if (*root)
+    {
+        if ((*root)->parent)
+        {
+            if ((*root)->parent->left == *root)
+                (*root)->parent->left = NULL;
+            else
+                (*root)->parent->right = NULL;
+            free(*root);
+        }
+        else
+        {
+            /* If the last node is the root itself */
+            free(*root);
+            *root = NULL;
+        }
+    }
 
-    *root = last_node;
+    /* Rebuild the heap */
+    if (*root)
+        heapify_down(*root);
 
-    heapify(*root, last_level, 0);
+    return extracted_value;
+}
 
-    return value;
+/**
+ * heapify_down - Rebuilds the Max Binary Heap after extracting the root.
+ * @node: Pointer to the root node of the heap.
+ */
+void heapify_down(heap_t *node)
+{
+    heap_t *largest = node;
+    heap_t *left = node->left;
+    heap_t *right = node->right;
+
+    if (left && left->n > largest->n)
+        largest = left;
+
+    if (right && right->n > largest->n)
+        largest = right;
+
+    if (largest != node)
+    {
+        swap_values(node, largest);
+        heapify_down(largest);
+    }
+}
+
+/**
+ * swap_values - Swaps the values of two heap nodes.
+ * @node1: First heap node.
+ * @node2: Second heap node.
+ */
+void swap_values(heap_t *node1, heap_t *node2)
+{
+    int temp = node1->n;
+    node1->n = node2->n;
+    node2->n = temp;
 }
