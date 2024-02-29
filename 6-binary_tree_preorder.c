0e300459149d4275e49a28c33e740b456f4c515c
Mohakad <mohkadtrd@gmail.com>
2024-02-29 16:32:55 +0300

    betty

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,11 +1,12 @@
 #include "binary_trees.h"
 /**
+ * binary_tree_preorder- preorder
  * @tree:  is a pointer to the root node
  * @func: pointer to func
 */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-    if (tree && func)
+	if (tree && func)
 	{
 		func(tree->n);
 		binary_tree_preorder(tree->left, func);
